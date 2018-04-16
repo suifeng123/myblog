2c9346d61558f3a9bbdc9dad332c19bb6b8d2569
suifeng123 <wswen1220@163.com>
2018-04-16 16:37:58 +0800

    asdf

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//var settings = require('./settings');
+var routes = require('./routes/index');
+var settings = require('./settings');
 var app = express();
+
+//add by wangshengwen
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port: settings.port
+  })
+}));
+
+routes(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +35,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ settings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2018/4/16.
+ */
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog',
+    host: 'localhost',
+    port: 27017
+};
