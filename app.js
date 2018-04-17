8852f34fd73d8311ba30e2f64864d2b731209d2d
suifeng123 <wswen1220@163.com>
2018-04-17 18:03:45 +0800

    提交关于session

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var settings = require('./settings');
+
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
-//var settings = require('./settings');
-var routes = require('./routes/index');
-var settings = require('./settings');
+
+
+var flash = require('connect-flash');
+
 var app = express();
 
-//add by wangshengwen
+app.use(flash());
+
 app.use(session({
   secret: settings.cookieSecret,
   key: settings.db,//cookie name
@@ -22,36 +29,19 @@ app.use(session({
     port: settings.port
   })
 }));
-
-routes(app);
-
-// view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+routes(app);
 
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 module.exports = function(app) {
+  var crypto = require('crypto'),
+      User = require('../models/user.js');
+
   app.get('/', function (req, res) {
     res.render('index', { title: '主页' });
   });
@@ -6,9 +9,54 @@ module.exports = function(app) {
     res.render('reg', { title: '注册' });
   });
   app.post('/reg', function (req, res) {
+       console.log("展示用户注册");
+       console.log(req);
+        console.log(req.query);
+    console.log(req.body);
+      //进行用户注册的信息的写入
+      var name = req.body.name;
+      var password = req.body.password;
+      var password_re = req.body['password-repeat'];
+
+    //首先检查用户两次输入的密码是否一致
+    if(password != password_re){
+      req.flash('error','两次输入密码不一致');
+      return res.redirect('/reg'); //重新跳转到登录页面
+    }
+
+    //生成密码的MD5值
+    var md5 = crypto.createHash('md5');
+       password = md5.update(req.body.password).digest('hex');
+    var newUser = new User({
+      name: name,
+      password: password,
+      email: req.body.email
+    });
+    //检查用户名是否已经存在
+    User.get(newUser.name,function(err,user){
+      if(err){
+        req.flash('error',err);
+        return res.redirect('/');
+      }
+
+      if(user){
+        req.flash('error','用户已存在');
+        return res.redirect('/reg'); //重新返回注册页
+      }
+      //如果不在在用户的话，就新增这个用户
+      newUser.save(function(err,user){
+        if(err){
+          req.flash('error',err);
+          return res.redirect('/reg');//注册失败，就重新返回注册页
+        }
+        req.session.user = user; //用户信息存入session中
+        req.flash('success','注册成功！');
+        res.redirect('/');//注册成功后返回主页
+      })
+    })
   });
   app.get('/login', function (req, res) {
-    res.render('login', { title: '登录' });
+    res.render('', { title: '登录' });
   });
   app.post('/login', function (req, res) {
   });
